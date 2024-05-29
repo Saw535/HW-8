90caf9a64d94cc0c3f9916ba4edc6cc1eee82392
Saw535 <Saw.535.rud@gmail.com>
2024-05-29 06:18:36 +0300

    Create GraphicsEditor.java

---
+++ src/GraphicsEditor.java
@@ -0,0 +1,7 @@
+// Клас для інтерфейсу графічного редактора
+class GraphicsEditor {
+    // Метод для виведення назви фігури
+    void printShapeName(Shape shape) {
+        System.out.println("Shape: " + shape.getName());
+    }
+}
